59cc57a32b093b900c1754178961c3e9ad8cadd2
unknown <qioixiy@gmail.com>
2014-05-13 13:33:19 +0800

    add db_conn.php

--- newsystem/src/login.php
+++ newsystem/src/login.php
@@ -1,2 +1,27 @@
-Welcome <?php echo $_POST["name"]; ?>.<br/>
-password <?php echo $_POST["password"]; ?>!
+<?php
+//if($_POST["action"]=="do")
+{
+	$name=$_POST["name"];
+	$pwd=$_POST["password"];
+
+	session_start();
+	include("db_conn.php");
+	$result=mysql_query("select count(*) from user where xh='$name' and pwd='$pwd'");
+	$row = mysql_fetch_array($result);
+			
+	if($row[0]==0) {
+		echo("<script language='javascript'>");
+		echo("alert('用户名或密码错误！');");
+		echo("window.location.href='../index.php';</script>");	
+		exit();
+	} else {
+		$_SESSION["xh"]=$xh;
+		echo "<script language='javascript'>"; 
+		echo " location='index.php';";
+		echo "</script>"; 	
+		exit();
+	}	
+}
+
+?>
+	

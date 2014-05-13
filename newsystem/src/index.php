846cfb1b04c3fbcedfcd9608f11562bd19a4c76b
unknown <qioixiy@gmail.com>
2014-05-13 17:57:56 +0800

    modify index.php

--- newsystem/src/index.php
+++ newsystem/src/index.php
@@ -2,22 +2,20 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
-	<title>管理系统</title>
+	<title>宿舍管理系统</title>
 	<link href="layout-main.css" rel="stylesheet" type="text/css" />
-	<meta name="Keywords" content="管理系统" />
-	<meta name="" content="标准之路,www.aa25.cn,网页标准布局,DIV+CSS" />
-	<meta name="author" content"×××,有问题请到www.68css.cn网站留言" />
-	<meta name="Description" content="本套网页标准布局模板是由标准之路（www.aa25.cn）制作完成，如果您要转载，请保留版权" />
+	<meta name="Keywords" content="宿舍管理系统" />
+	<meta name="" content="DIV+CSS" />
+	<meta name="author" content="qioixiy" />
+	<meta name="Description" content="" />
   </head>
 
   <body>
 	<div id="container">
-	  <div id="header">This is the Header</div>
-	  <br class="clearfloat" />
-	  <div id="menu">This is the Menu</div>
-	  <br class="clearfloat" />
+	  <div id="header"><h1>This is the Header</h1></div>
+
 	  <div id="mainContent">
-		<div id="sidebar">导航栏<br/>
+		<div id="sidebar"><p>导航栏</p>
 		  <ul>
 			<li><a href="" target="_blank">首页</a></li>
 			<li><a href="" target="_blank">寝室查询</a></li>
@@ -28,14 +26,13 @@
 			<li><a href="" target="_blank">个人资料</a></li>
 			<li><a href="" target="_blank">注销系统</a></li>
 		  </ul>
-		  <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 		</div>
-		<div id="content">2列左侧固定右侧自适应宽度，未知高度+头部+导航+尾部<a href="http://www.aa25.cn">标准之路www.aa25.cn</a>
-		  <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<div id="content">
+		  <p>test</p>
 		</div>
 	  </div>
 	  <br class="clearfloat" />
-	  <div id="footer">This is the footer<span style="display:none"></span></div><br/><br/><br/><br/><br/>
+	  <div id="footer">This is the footer<span style="display:none"></span></div>
 	</div>
   </body>
 </html>

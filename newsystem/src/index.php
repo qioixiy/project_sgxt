c623173107e0a45f5564c052594be08670213333
qioixiy <qioixiy@gmail.com>
2014-05-18 16:50:45 -0700

    split php file.

--- newsystem/src/index.php
+++ newsystem/src/index.php
@@ -1,35 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
-    <title>宿舍管理系统</title>
-    <link href="layout.css" rel="stylesheet" type="text/css" />
-    <meta name="Keywords" content="宿舍管理系统" />
-    <meta name="" content="DIV+CSS" />
-    <meta name="author" content="qioixiy" />
-    <meta name="Description" content="" />
-  </head>
-  
-  <body>
-    <div class="header"><h1 align="center">宿舍管理系统</h1></div>
-    <div class="maincontent">
-      <div class="sidebar"><h4>导航栏</h4></br>
-	<ul>
-	  <!--<li><a href="" target="_blank">首页</a></li>-->
-	  <li><a href="">首页</a></li>
-	  <li><a href="">寝室查询</a></li>
-	  <li><a href="">班级查询</a></li>
-	  <li><a href="">文明寝室</a></li>
-	  <li><a href="">损坏登记</a></li>
-	  <li><a href="">评分信息</a></li>
-	  <li><a href="">个人资料</a></li>
-	  <li><a href="">注销系统</a></li>
-	</ul>
-      </div>
-      <div class="content">
-	<?php include "table.php" ?>
-      </div>
-    </div>
-    <div class="footer">This is the footer<span style="display:none"></span></div>
-  </body>
-</html>
+<?php include "header.php" ?>
+<?php include "table.php" ?>
+<?php include "footer.php" ?>

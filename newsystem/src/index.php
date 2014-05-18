8413c5f1762bdc36315b869310b554479a457fb4
qioixiy <qioixiy@gmail.com>
2014-05-18 11:36:35 -0700

    layout redone

--- newsystem/src/index.php
+++ newsystem/src/index.php
@@ -1,40 +1,35 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
-	<title>宿舍管理系统</title>
-	<link href="layout-main.css" rel="stylesheet" type="text/css" />
-	<meta name="Keywords" content="宿舍管理系统" />
-	<meta name="" content="DIV+CSS" />
-	<meta name="author" content="qioixiy" />
-	<meta name="Description" content="" />
+    <meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+    <title>宿舍管理系统</title>
+    <link href="layout.css" rel="stylesheet" type="text/css" />
+    <meta name="Keywords" content="宿舍管理系统" />
+    <meta name="" content="DIV+CSS" />
+    <meta name="author" content="qioixiy" />
+    <meta name="Description" content="" />
   </head>
-
+  
   <body>
-	<div id="container">
-	  <div id="header"><h1 align="center">宿舍管理系统</h1></div>
-
-	  <div id="mainContent">
-		<div id="sidebar"><p>导航栏</p>
-		  <ul>
-			<!--<li><a href="" target="_blank">首页</a></li>-->
-			<li><a href="">首页</a></li>
-			<li><a href="" target="_blank">寝室查询</a></li>
-			<li><a href="" target="_blank">班级查询</a></li>
-			<li><a href="" target="_blank">文明寝室</a></li>
-			<li><a href="" target="_blank">损坏登记</a></li>
-			<li><a href="" target="_blank">评分信息</a></li>
-			<li><a href="" target="_blank">个人资料</a></li>
-			<li><a href="" target="_blank">注销系统</a></li>
-			</br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br></br>
-		  </ul>
-		</div>
-		<div id="content">
-		  <p>test</p>
-		</div>
-	  </div>
-	  <br class="clearfloat" />
-	  <div id="footer">This is the footer<span style="display:none"></span></div>
-	</div>
+    <div class="header"><h2 align="center">宿舍管理系统</h1></div>
+    <div class="maincontent">
+      <div class="sidebar"><p>导航栏</p>
+	<ul>
+	  <!--<li><a href="" target="_blank">首页</a></li>-->
+	  <li><a href="">首页</a></li>
+	  <li><a href="" target="_blank">寝室查询</a></li>
+	  <li><a href="" target="_blank">班级查询</a></li>
+	  <li><a href="" target="_blank">文明寝室</a></li>
+	  <li><a href="" target="_blank">损坏登记</a></li>
+	  <li><a href="" target="_blank">评分信息</a></li>
+	  <li><a href="" target="_blank">个人资料</a></li>
+	  <li><a href="" target="_blank">注销系统</a></li>
+	</ul>
+      </div>
+      <div class="content">
+	<p>test</p>
+      </div>
+    </div>
+    <div class="footer">This is the footer<span style="display:none"></span></div>
   </body>
 </html>

ff4aedca5206a4c5e293d3b25c6cf73d2ce2b9a6
qioixiy <qioixiy@gmail.com>
2014-05-18 20:47:56 -0700

    mysql中文支持，宿舍table初始化。

--- newsystem/src/Dormitory_manager/list.php
+++ newsystem/src/Dormitory_manager/list.php
@@ -3,7 +3,40 @@
 include("../common/header.php");
 include("../common/db_conn.php");
 
+$result = mysql_query("SELECT * FROM students");
+?>   
+<html>
+  <head>
+    <link href="/sgxt/newsystem/assert/css/table_customers.css" rel="stylesheet" type="text/css" />
+  </head>
+  
+  <body>
+    <table id="customers">
+      <tr>
+	<th>ID</th>
+	<th>名字</th>
+	<th>联系方式</th>
+	<th>创建时间</th>
+      </tr>
+<?php
+    $index = 0;
+    while($row = mysql_fetch_array($result)) {
+        if ($index == 0) {
+            echo "<tr>"; 
+            $index = 1;
+        } else {
+            echo "<tr class='alt'>"; 
+            $index = 0;
+        }
+        echo "<td>" . $row['id'] . "</td>" . "<td>" . $row['name'] . "</td>" . "<td>" . $row['tel'] . "</td>" . "<td>" . $row['created'] . "</td>";
+        echo "</tr>";
+    }
+?>      
+     
+    </table>
+  </body>
+</html>
 
-
+<?php
 include("../common/footer.php");
 ?>
--- newsystem/src/common/db_conn.php
+++ newsystem/src/common/db_conn.php
@@ -3,4 +3,6 @@ $lnk = mysql_connect('localhost', 'root', '')
     or die ('Not connected : ' . mysql_error());
 mysql_select_db('manager_db', $lnk)
     or die ('Can\'t use db : ' . mysql_error());
+//support chinese
+mysql_query("SET NAMES GBK");
 ?>

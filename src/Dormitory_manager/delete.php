ff295559f669cabf85226aa51a9788c775d76232
qioixiy <qioixiy@gmail.com>
2014-05-25 16:51:10 -0700

    add delete.php js handler

--- src/Dormitory_manager/delete.php
+++ src/Dormitory_manager/delete.php
@@ -6,13 +6,21 @@ include_once("../common/db_conn.php");
 <html>
   <head>
     <link href="/sgxt/assert/css/table_customers.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript">
+function delete_select() {
+	
+}
+function reset_select() {
+	
+}
+	</script>
   </head>
   
   <body>
 	<div>
 	  <div style='position:absolute;top:10px;right:30px;width:100px;height:80px;border:1px' id='float-div'>
-		<button type="button">删除选择项</button>
-		<button type="button">清除选择项</button>
+		<button type="button" onclick=reset_select()>复位选择项</button>
+		<button type="button" onclick=delete_select()>删除选择项</button>
 	  </div>
 	</div>
 	<div class="list" style="width:90%;">

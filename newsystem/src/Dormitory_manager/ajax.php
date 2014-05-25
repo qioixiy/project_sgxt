9471fa574522ad1fb1c6fcff5566b9c0aa0d278b
qioixiy <qioixiy@gmail.com>
2014-05-24 19:01:26 -0700

    code indent

--- newsystem/src/Dormitory_manager/ajax.php
+++ newsystem/src/Dormitory_manager/ajax.php
@@ -3,12 +3,8 @@ include_once("../common/db_conn.php");
 
 header("Content-Type:text/html;charset=gb2312");
 
-//get parameters from URL
-$susheleixing = $_GET["susheleixing"];
-$sushelou = $_GET["sushelou"];
-$ruzhurenshu = $_GET["ruzhurenshu"];
-
-echo <<<EOT
+function Dorms_view() {
+	echo <<<EOT
 <table id="customers">
        <tr>
 		  <th>ID</th>
@@ -23,7 +19,7 @@ echo <<<EOT
 EOT;
 
 	$result = mysql_query("SELECT * FROM dorms");
-
+	
     $index = 0;
     while($row = mysql_fetch_array($result)) {
         if ($index == 0) {
@@ -43,9 +39,16 @@ EOT;
 			. "<td>" . $row['±¸×¢'] . "</td>";
         echo "</tr>";
     }
-     
-echo <<<EOF
+	
+	echo <<<EOF
 	  </table>
-
 EOF;
+}
+
+//get parameters from URL
+$susheleixing = $_GET["susheleixing"];
+$sushelou = $_GET["sushelou"];
+$ruzhurenshu = $_GET["ruzhurenshu"];
+Dorms_view();
+
 ?>
